320c42935271ae26dee01833502ee295b19f2c14
Dmitryi <ilacker106@gmail.com>
2024-02-04 21:55:13 +0700

    echo bot

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(1)
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN_API = "6911956163:AAEHbnfX9lO78oBGqTOZwWJI0oSUx66e2Lk"
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
